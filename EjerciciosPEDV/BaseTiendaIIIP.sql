b4bee130169e7e993b75bba2e0a6e5f88b3e9b37
Hector Castellanos <osoriohector89@gmail.com>
2020-07-20 14:07:36 -0600

    Funcion Buscar y Modificar y demas funciones

--- EjerciciosPEDV/BaseTiendaIIIP.sql
+++ EjerciciosPEDV/BaseTiendaIIIP.sql
@@ -49,64 +49,6 @@ create procedure modificarUsuario(
 AS 
 BEGIN
 if exists (select nombreUsuario,idUsuario from Usuario where (nombreUsuario=@nombreUsuario and idUsuario=@idUsuario and estado = 'Activo'))
-raiserror ('Usuario en uso, Utiliza otro por favor',16,1)
-update Usuario set nombre = @nombre, apellido = @apellido, psw = @psw, rol = @rol, correo = @correo
-where idUsuario = @idUsuario
+raiserror ('Usuario en uso, Utiliza otro por favor')
+update 
 END
-
-Create procedure eliminarUsuario(
-@idUsuario int, @rol char(15) 
-)
-AS
-BEGIN
-if exists (select nombreUsuario from Usuario where @rol = 'admin')
-	raiserror('El usuario *admin*, No se puede eliminar, accion denegada',16,1)
-	else
-	update Usuario set estado = 'Eliminado'
-	where idUsuario = @idUsuario and rol <> 'admin'
-END
-
-create master key encryption by
-password = 'SimplePassword';
-go
-
-create certificate TiendaIIIPHOC01
-with subject='TiendaIIIPHOC';
-go
-
-select * from sys.certificates
-go
-
-create symmetric key psw_key_01
-with algorithm = triple_des
-encryption by certificate TiendaIIIPHOC01;
-go
-
-alter table Usuario
-add Passwd varbinary(128);
-
-open symmetric key psw_key_01
-decryption by certificate TiendaIIIPHOC01;
-
-update Usuario
-set Passwd = ENCRYPTBYKEY(KEY_GUID('psw_key_01'),convert(nvarchar(20),psw));
-
-close symmetric key psw_key_01;
-
-go
-
-
-open symmetric key psw_key_01
-decryption by certificate TiendaIIIPHOC01;
-
-select convert(nvarchar(20),DECRYPTBYKEY(Passwd)) as [psw descifrado], Passwd from Usuario
-close symmetryc key psw_key_01;
-
-create procedure BuscarUsuario(@userName varchar(50))
-as
-begin
-select CONCAT(nombre, ' ', apellido) as 'Nombre Completo', nombreUsuario as 'Usuario',
-estado as 'Estado', rol as 'Puesto', correo as 'correo'
-from Usuario
-where nombreUsuario like '%' +@userName+ '%'
-end
